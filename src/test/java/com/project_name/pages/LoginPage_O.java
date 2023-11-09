96291046ef39ecfe7646a76d509ab3c240c6d522
CrazyAlvin <alvinalvares.it@gmail.com>
2023-11-09 11:33:38 -0500

    first commit

--- src/test/java/com/project_name/pages/LoginPage_O.java
+++ src/test/java/com/project_name/pages/LoginPage_O.java
@@ -2,6 +2,29 @@
 
 package com.project_name.pages;
 
+import com.project_name.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage_O extends BasePage{
+
+    @FindBy(xpath = "//input[@data-qa='login-email']")
+    public WebElement emailBox;
+
+
+    @FindBy(xpath = "//input[@data-qa='login-password']")
+    public WebElement passwordBox;
+
+
+    @FindBy(xpath = "//button[@data-qa='login-button']")
+    public WebElement loginButton;
+
+
+    public void login(){
+
+        emailBox.sendKeys(ConfigurationReader.getProperty("username"));
+        passwordBox.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
+    }
 
 }
--- src/test/java/com/project_name/step_definitions/LoginStepDefs_O.java
+++ src/test/java/com/project_name/step_definitions/LoginStepDefs_O.java
@@ -1,7 +1,30 @@
 package com.project_name.step_definitions;
 
+import com.project_name.pages.LoginPage_O;
+import com.project_name.utilities.ConfigurationReader;
+import com.project_name.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class LoginStepDefs_O {
 
+    LoginPage_O loginPage = new LoginPage_O();
+    @Given("User is on the login page")
+    public void user_is_on_the_login_page() {
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+    @When("User logged is with valid credential")
+    public void user_logged_is_with_valid_credential() {
+        loginPage.login();
+    }
+    @Then("User should see this title {string}")
+    public void user_should_see_this_title(String expectedTitle) {
+
+        Assert.assertEquals(expectedTitle,Driver.getDriver().getTitle());
+
+    }
 }
 
